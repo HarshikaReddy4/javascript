08eec54506f710121913c1dd062efd7f9bc84243
Harshika Reddy <121601637+HarshikaReddy4@users.noreply.github.com>
2024-03-02 14:29:23 +0530

    Create code.js
    Operations in js

---
+++ code.js
@@ -0,0 +1,11 @@
+var sum = 10 + 10;
+var diff = 10 - 10;
+var mul = 2* 8;
+var quo = 2/1;
+var rem = 2%1;
+console.log(sum);
+console.log(diff);
+console.log(mul);
+console.log(quo);
+console.log(rem);
+
